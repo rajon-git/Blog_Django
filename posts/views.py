98a875f340858f85f9ed6b4acb36ac7f96f99a9f
rajon-git <rajon.zhsust15@gmail.com>
2024-07-01 18:40:35 +0600

    class base view add post create

--- posts/views.py
+++ posts/views.py
@@ -2,19 +2,33 @@ from django.shortcuts import render,redirect
 from . import forms
 from . import models
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
-@login_required
-def add_post(request):
-    if request.method == 'POST':
-        post_forms = forms.PostForm(request.POST)
-        if post_forms.is_valid():
-            post_forms.instance.author = request.user
-            post_forms.save()
-            return redirect('homepage')
-    else:
-        post_forms = forms.PostForm()
-    return render(request, 'add_post.html',{'form': post_forms})
+
+# @login_required
+# def add_post(request):
+#     if request.method == 'POST':
+#         post_forms = forms.PostForm(request.POST)
+#         if post_forms.is_valid():
+#             post_forms.instance.author = request.user
+#             post_forms.save()
+#             return redirect('homepage')
+#     else:
+#         post_forms = forms.PostForm()
+#     return render(request, 'add_post.html',{'form': post_forms})
+
+#add post class based view 
+class AddPosTCreateView(CreateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    success_url = reverse_lazy('homepage')
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 @login_required
 def edit_post(request,id):
