c74991a279610373db972090336f74094379c1c9
rajon-git <rajon.zhsust15@gmail.com>
2024-07-02 14:38:48 +0600

    media file added

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,8 @@ urlpatterns = [
     path('author/', include('author.urls')),
     path('post/', include('posts.urls')),
     path('category/', include('categories.urls')),
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     category = models.ManyToManyField(Category) #ekta post multiple category te thakte pare abar ekta categoryte multiple post thakte pare
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/', blank= True, null=True)
 
     def __str__(self):
         return self.title
